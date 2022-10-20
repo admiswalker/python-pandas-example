6986686c9cd962caa041a10fb0001467c5599c6e
admiswalker <admiswalker@gmail.com>
2022-10-21 00:18:45 +0900

    add ex2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import io
 import pandas as pd
 import numpy as np
 
-def main():
+def main01():
     print('hello')
 
     df_data = pd.DataFrame([[111, 'aaa', 100, 123, 987],
@@ -22,12 +22,12 @@ def main():
     #df_data[l_col[0]+'_TF'] = eval(f"np.where(df_data[l_col[0]] {l_is_sig[0]} l_val[0], 1, 0)")
     #print(df_data)
     #print()
-
+    
     for i in range(len(l_col)):
         df_data[l_col[i]+'_TF'] = eval(f"np.where(df_data[l_col[i]] {l_is_sig[i]} l_val[i], 1, 0)")
     print(df_data)
     print()
-
+    
     # sum
     #df_data['TF_sum'] = df_data[l_col[0]+'_TF'] + df_data[l_col[1]+'_TF'] + df_data[l_col[2]+'_TF']
     #print(df_data)
@@ -40,5 +40,35 @@ def main():
     print(df_data)
     print()
     
+def main02():
+    print('main02')
     
-main()
+    df_data = pd.DataFrame([[111, 'aaa', 100, 123, 987],
+                            [222, 'bbb', 200, 321, 777]],
+                           columns=['user_id', 'user_name', 'param_1', 'param_2', 'param_3'])
+    print(df_data)
+    print()
+
+    l_col = ['param_1', 'param_2', 'param_3']
+    l_is_sig = ['>=', '>=', '>=']
+    l_val = [150, 150, 150]
+
+    # sum
+    #df_data['TF_sum'] = int(0)
+    #for i in range(len(l_col)):
+    #    df_data['TF_sum'] += np.where(df_data[l_col[i]] >= l_val[i], 1, 0)
+    #print(df_data)
+    #print()
+
+    # sum
+    df_data['TF_sum'] = int(0)
+    for i in range(len(l_col)):
+        df_data['TF_sum'] += eval(f"np.where(df_data[l_col[i]] {l_is_sig[i]} l_val[i], 1, 0)")
+    print(df_data)
+    print()
+
+    # ex: np.where's return value:
+    print(np.where(df_data[l_col[i]] >= l_val[i], 1, 0))
+
+#main01()
+main02()
